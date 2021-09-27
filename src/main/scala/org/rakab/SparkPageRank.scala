fcdc08b3c5b0371e0913493d638dc2e136c6549d
Rahul Choudhary <rahulc.0212@gmail.com>
2021-09-26 23:54:44 -0500

    write to csv output

--- src/main/scala/org/rakab/SparkPageRank.scala
+++ src/main/scala/org/rakab/SparkPageRank.scala
@@ -8,20 +8,19 @@
 
 package org.rakab
 
-import com.typesafe.config.ConfigFactory
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql.functions.collect_list
+import org.apache.spark.sql.{Encoders, Row, SaveMode, SparkSession}
 
 object SparkPageRank {
 
   case class Links(FromNodeId: String, ToNodeId: String)
 
   def main(args: Array[String]) = {
-    var iterations = 20
+    val log = LogManager.getRootLogger()
+    log.setLevel(Level.INFO)
 
-    if(args.length >= 1) {
-      iterations = args(0).toInt
-    }
+    log.info("Building Spark Session")
 
     val spark = SparkSession
       .builder()
@@ -29,33 +28,63 @@ object SparkPageRank {
       .config("spark.master", "local")
       .getOrCreate()
 
-    val config = ConfigFactory.load("PageRank.conf")
+    // Take paths from the user as command line arguments
+    val inputPath = args(0)
+    val outputPath = args(1)
+
+    var iterations = 10
+
+    if(args.length >= 3) {
+      iterations = args(2).toInt
+    }
+
+    log.info(s"Loaded config inputPath->$inputPath, outputPath->$outputPath")
+    log.info(s"Total iterations = $iterations")
 
     import spark.implicits._
-    val ds = spark.read.text(config.getString("pageRank.inputFile")).as[String]
+    log.info("Reading data into df")
+    // Create schema object to read the data
+    val linkSchema = Encoders.product[Links].schema
+    // Read csv data into the dataframe
+    var df = spark.read.schema(linkSchema).option("header", false).option("delimiter", "\t").csv(inputPath)
+
+    // Filtering out any null values
+    df = df
+      .filter(row => {
+        row != null && row.getString(0) != null && row.getString(1) != null
+      }) // Trim any whitespaces before or after the values
+      .map(r => (r.getString(0).toLowerCase().trim, r.getString(1).toLowerCase().trim))
+      .filter(r => r._2.contains("category:") || !r._2.contains(":"))
+      .toDF("_1", "_2")
+
+    log.info("Creating links")
 
     // Dataset that maintains each url and its list of outgoing links
-    val linksList = ds
-      .map[(String, String)]((row: String) => {
-        val edge = row.split("\\s+")
-        (edge(0), edge(1))
-      })
+    val linksList = df
       .dropDuplicates()
       .groupBy("_1")
       .agg(collect_list("_2") as "links")
 
+    log.info("Creating initial ranks")
+
     // Dataset to keep track of urls and their corresponding ranks. Initialise the rank of each url to 1
     var ranks = linksList.map[(String, Double)]((row: Row) => (row.getString(0), 1.toDouble))
+
+//    linksList.show()
+//    ranks.show()
 
     //Time the for loop
     val t1 = System.nanoTime()
 
+    log.info("Starting iterations")
+
     for(i <- 1 to iterations) {
+      log.info(s"Creating intmRanks ${i}")
       var updatedRanks = linksList
         .join(ranks, "_1")
         .flatMap(row => {
           val sz = row.getSeq(1).length
-          row.getSeq[String](1).map(link => (link, row.getDouble(2) / sz))
+          row.getSeq[String](1).map(link => (link, row.getDouble(2) / sz)) ++ Seq[(String, Double)]((row.getString(0), 0))
         })
         .groupBy($"_1")
         .sum("_2")
@@ -64,7 +93,15 @@ object SparkPageRank {
       ranks = updatedRanks
     }
 
-    ranks.show()
+//    ranks.show()
+    log.info(s"Creating final ranks of all left side articles")
+
+    var finalRanks = linksList.join(ranks, Seq("_1"), "left_outer").drop("links")
+//    finalRanks.show()
+
+    log.info(s"Writing to output $outputPath")
+
+    finalRanks.write.mode(SaveMode.Overwrite).option("headers", true).csv(outputPath)
 
     val duration = (System.nanoTime() - t1) / 1e9d
     println("Time taken to run " + iterations + " iterations = " + duration + " seconds")
